05a7d0e8fda38ecf685961ef069909541811fa49
Bernie Gray <bfgray3@gmail.com>
2022-11-29 22:11:34 -0500

    tweak utils.h

--- include/utils.h
+++ include/utils.h
@@ -5,29 +5,27 @@
 #include <string>
 #include <vector>
 
-// TODO: can use braced initialization?
-
 template <typename T>
 std::vector<T> read_vector_from_file(const std::string &filename) {
-  std::ifstream is(filename);
-  std::istream_iterator<T> start(is), end;
+  std::ifstream input_file_stream{filename};
+  std::istream_iterator<T> start{input_file_stream}, end;
   return {start, end};
 }
 
 template <typename T>
 void write_vector_to_file(const std::vector<T> &v,
                           const std::string &filename) {
-  std::ofstream output_file(filename);
-  std::ostream_iterator<T> output_iterator(output_file, "\n");
-  std::copy(std::begin(v), std::end(v), output_iterator);
+  std::ofstream output_file_stream{filename};
+  std::ostream_iterator<T> output_stream_iterator{output_file_stream, "\n"};
+  std::copy(std::begin(v), std::end(v), output_stream_iterator);
 }
 
 template <typename T> std::vector<T> read_vector_from_stdin() {
-  std::istream_iterator<T> start(std::cin), end;
+  std::istream_iterator<T> start{std::cin}, end;
   return {start, end};
 }
 
 template <typename T> void write_vector_to_stdout(const std::vector<T> &v) {
   std::copy(std::begin(v), std::end(v),
-            std::ostream_iterator<T>(std::cout, "\n"));
+            std::ostream_iterator<T>{std::cout, "\n"});
 }
