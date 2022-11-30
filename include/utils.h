43ec280512cb12a2aef96355893ef0e7096f2d24
Bernie Gray <bfgray3@gmail.com>
2022-11-29 21:47:25 -0500

    add include/utils.h and hook updates

---
+++ include/utils.h
@@ -0,0 +1,35 @@
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <string>
+#include <vector>
+
+// TODO: can use string_view?
+// TODO: can use braced initialization?
+
+template <typename T>
+std::vector<T> read_vector_from_file(const std::string &filename) {
+  std::ifstream is(filename);
+  std::istream_iterator<T> start(is), end;
+  return {start, end};
+}
+
+template <typename T>
+void write_vector_to_file(const std::vector<T> &v,
+                          const std::string &filename) {
+  std::ofstream output_file(filename);
+  std::ostream_iterator<T> output_iterator(output_file, "\n");
+  std::copy(std::begin(v), std::end(v), output_iterator);
+}
+
+template <typename T> std::vector<T> read_vector_from_stdin() {
+  std::istream_iterator<T> iterBegin(std::cin), iterEnd;
+  return {iterBegin, iterEnd};
+}
+
+template <typename T> void write_vector_to_stdout(const std::vector<T> &v) {
+  std::copy(std::begin(v), std::end(v),
+            std::ostream_iterator<T>(std::cout, " "));
+  std::cout << '\n';
+}
