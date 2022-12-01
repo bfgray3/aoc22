82e6202ce22085326b6b2aeb6442ee174418bf95
Bernie Gray <bfgray3@gmail.com>
2022-11-30 22:03:26 -0500

    add utils namespace

--- 01/main.cpp
+++ 01/main.cpp
@@ -3,8 +3,8 @@
 // test with  make clean && make day=01 && ./main <<< "1 2 3"
 
 int main() {
-  auto v2 = read_vector_from_stdin<int>();
-  write_vector_to_file(v2, "asdf");
-  auto v3 = read_vector_from_file<int>("asdf");
-  write_vector_to_stdout(v3);
+  auto v2 = utils::read_vector_from_stdin<int>();
+  utils::write_vector_to_file(v2, "asdf");
+  auto v3 = utils::read_vector_from_file<int>("asdf");
+  utils::write_vector_to_stdout(v3);
 }
--- include/utils.h
+++ include/utils.h
@@ -7,6 +7,8 @@
 #include <string>
 #include <vector>
 
+namespace utils {
+
 template <typename T>
 std::vector<T> read_vector_from_file(const std::string &filename) {
   std::ifstream input_file_stream{filename};
@@ -31,3 +33,5 @@ template <typename T> void write_vector_to_stdout(const std::vector<T> &v) {
   std::copy(std::begin(v), std::end(v),
             std::ostream_iterator<T>{std::cout, "\n"});
 }
+
+} // namespace utils
