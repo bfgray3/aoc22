22fd60744e018f014517e0e8d2d4f4abbd96d130
Bernie Gray <bfgray3@gmail.com>
2022-11-30 21:59:24 -0500

    cleanup

--- 01/main.cpp
+++ 01/main.cpp
@@ -3,8 +3,8 @@
 // test with  make clean && make day=01 && ./main <<< "1 2 3"
 
 int main() {
-  std::vector<int> v2 = read_vector_from_stdin<int>();
+  auto v2 = read_vector_from_stdin<int>();
   write_vector_to_file(v2, "asdf");
-  std::vector<int> v3 = read_vector_from_file<int>("asdf");
+  auto v3 = read_vector_from_file<int>("asdf");
   write_vector_to_stdout(v3);
 }
