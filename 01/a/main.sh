07a334f65ede78c8b4abb6d2d23f797013adcf06
Bernie Gray <bfgray3@gmail.com>
2022-12-02 23:03:14 -0500

    1a in bash

---
+++ 01/a/main.sh
@@ -0,0 +1,19 @@
+max_elf_cals=0
+current_elf_cals=0
+
+
+while read cal
+do
+  if [[ -z $cal ]]
+  then
+    if [[ $current_elf_cals -gt $max_elf_cals ]]
+    then
+      max_elf_cals=$current_elf_cals
+    fi
+    current_elf_cals=0
+  else
+    ((current_elf_cals+=cal))
+  fi
+done < input.txt
+
+echo $max_elf_cals
