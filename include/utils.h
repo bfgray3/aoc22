5ecd185c87a176c3abfcfea56f34bd30dfff8e1e
Bernie Gray <bfgray3@gmail.com>
2022-12-02 23:14:03 -0500

    formatting

--- include/utils.h
+++ include/utils.h
@@ -17,21 +17,21 @@ std::vector<T> read_vector_from_file(const std::string &filename) {
 }
 
 template <typename T>
-void write_vector_to_file(const std::vector<T> &v,
-                          const std::string &filename) {
+void write_vector_to_file(const std::vector<T> &v, const std::string &filename) {
   std::ofstream output_file_stream{filename};
   std::ostream_iterator<T> output_stream_iterator{output_file_stream, "\n"};
   std::copy(std::begin(v), std::end(v), output_stream_iterator);
 }
 
-template <typename T> std::vector<T> read_vector_from_stdin() {
+template <typename T>
+std::vector<T> read_vector_from_stdin() {
   std::istream_iterator<T> start{std::cin}, end;
   return {start, end};
 }
 
-template <typename T> void write_vector_to_stdout(const std::vector<T> &v) {
-  std::copy(std::begin(v), std::end(v),
-            std::ostream_iterator<T>{std::cout, "\n"});
+template <typename T>
+void write_vector_to_stdout(const std::vector<T> &v) {
+  std::copy(std::begin(v), std::end(v), std::ostream_iterator<T>{std::cout, "\n"});
 }
 
 } // namespace utils
