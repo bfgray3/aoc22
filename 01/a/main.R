df6769d4e762280298cf00431abfaeb3a8a1f9d5
Bernie Gray <bfgray3@gmail.com>
2022-12-02 23:14:43 -0500

    use integers in r

--- 01/a/main.R
+++ 01/a/main.R
@@ -1,13 +1,13 @@
 cals <- readLines("input.txt")
 
-max_elf_cals <- current_elf_cals <- 0
+max_elf_cals <- current_elf_cals <- 0L
 
 for (cal in cals) {
   if (identical(cal, "")) {
     if (current_elf_cals > max_elf_cals) {
       max_elf_cals <- current_elf_cals
     }
-      current_elf_cals <- 0
+      current_elf_cals <- 0L
   } else {
     current_elf_cals <- current_elf_cals + as.integer(cal)
   }
