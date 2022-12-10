e6b2608ceb84c55cf20423e2e61d1da8a7d3c0fa
Bernie Gray <bfgray3@gmail.com>
2022-12-10 11:16:22 -0500

    get rid of parens

--- 08/a/main.py
+++ 08/a/main.py
@@ -9,10 +9,8 @@ print(
     (
         (np.roll(np.maximum.accumulate(grid, axis=0), shift=1, axis=0) < grid)
         | (np.roll(np.maximum.accumulate(grid, axis=1), shift=1, axis=1) < grid)
-        | (
-            (np.roll(np.maximum.accumulate(grid[::-1,:], axis=0), shift=1, axis=0) < grid[::-1,:])[::-1,:]
-            | (np.roll(np.maximum.accumulate(grid[:,::-1], axis=1), shift=1, axis=1) < grid[:,::-1])[:, ::-1]
-        )
+        | (np.roll(np.maximum.accumulate(grid[::-1, :], axis=0), shift=1, axis=0) < grid[::-1, :])[::-1, :]
+        | (np.roll(np.maximum.accumulate(grid[:, ::-1], axis=1), shift=1, axis=1) < grid[:, ::-1])[:, ::-1]
     )[1:-1, 1:-1].sum()
-  + sum(2 * s for s in grid.shape) - 4
+    + sum(2 * s for s in grid.shape) - 4
 )
