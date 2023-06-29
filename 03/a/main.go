73410fadc74cdfcc55ab093ff984c4bc091c978c
Bernie Gray <bfgray3@gmail.com>
2023-06-28 23:33:11 -0400

    cleanup

--- 03/a/main.go
+++ 03/a/main.go
@@ -8,21 +8,18 @@ import (
 	"os"
 )
 
+var sentinel any
+
 // TODO: make the other literals globals
 func priority(c byte) (byte, error) {
 	if c >= byte('a') && c <= byte('z') {
 		return c - byte('a') + 1, nil
 	} else if c >= byte('A') && c <= byte('Z') {
-		return c - 2 * byte('A') + byte('Z') + 2, nil
+		return c - 2*byte('A') + byte('Z') + 2, nil
 	} else {
 		return 0, errors.New("bad input")
 	}
 }
-
-
-type void struct{}
-var sentinel void
-
 
 func main() {
 	file, err := os.Open(os.Args[1])
@@ -44,17 +41,17 @@ func main() {
 	var intersection byte
 
 	for _, r := range rucksacks {
-		c1 := make(map[byte]struct{})
-		c2 := make(map[byte]struct{})  // TODO: any??
+		c1 := make(map[byte]any)
+		c2 := make(map[byte]any)
 
-		for i := 0; i < len(r) / 2; i++ {
-			c1[byte(r[i])] = sentinel
+		for i := 0; i < len(r)/2; i++ {
+			c1[r[i]] = sentinel
 		}
 		for i := len(r) / 2; i < len(r); i++ {
-			c2[byte(r[i])] = sentinel
+			c2[r[i]] = sentinel
 		}
 
-		for k := range(c1) {
+		for k := range c1 {
 			if _, ok := c2[k]; ok {
 				intersection = k
 				break
@@ -62,13 +59,14 @@ func main() {
 		}
 		rPriority, err := priority(intersection)
 		if err != nil {
+			log.Fatal("error finding priority")
 		}
 		priorities = append(priorities, rPriority)
 	}
 
-	answer := 0
-	for _, p := range(priorities) {
-		answer += int(p)
+	var answer uint
+	for _, p := range priorities {
+		answer += uint(p)
 	}
 
 	fmt.Println(answer)
