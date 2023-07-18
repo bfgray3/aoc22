78e575f930d956be1eb567f8d65c9ce2c3adbf76
Bernie Gray <bfgray3@gmail.com>
2023-07-17 22:34:34 -0400

    add test_part()

--- test.sh
+++ test.sh
@@ -4,20 +4,42 @@ set -eu
 
 make -s clean
 
+function test_part {
+  local day part input_file correct_answer f
+
+  day=$1
+  part=$2
+
+  input_file="./$day/input.txt"
+
+  if [[ ! -d "$day/$part" ]]
+  then
+    return
+  fi
+
+  correct_answer=$(jq -r ".ans$day.$part" answers.json)
+
+  for f in "$day/$part"/*
+  do
+    test_one "$f" "$correct_answer"
+  done
+}
+
 function test_one {
-  local f correct_answer day part extension ans  # TODO: remove day, part
+  local f correct_answer extension ans
+  # TODO: make input_file local too
 
   f=$1
   correct_answer=$2
-  day=$3  # TODO: remove
-  part=$4  # TODO: remove
 
   extension="${f##*.}"
 
   case $extension in
     cpp)
-      make -s day="$day" part="$part"  # TODO: clean this up to take just the directory so we don't need day and part
-      ans=$("./$d/$p/aocmain" "$input_file")
+      echo cpp
+      #make -s day="$day" part="$part"  # TODO: clean this up to take just the directory so we don't need day and part
+      #ans=$("./$d/$p/aocmain" "$input_file")
+      return
       ;;
     py)
       ans=$(python3 "$f" "$input_file")
@@ -39,8 +61,10 @@ function test_one {
 
   if [[ "$ans" = "$correct_answer" ]]
   then
+    # FIXME: d/p not available anymore
     echo "day $d part $p correct: $extension"
   else
+    # FIXME: d/p not available anymore
     echo "day $d part $p incorrect: $extension; got $ans and expected $correct_answer" >&2
     exit 1
   fi
@@ -49,23 +73,14 @@ function test_one {
 
 if [[ $# -eq 1 ]]  # TODO: number
 then
-  test_one file answer day part
+  test_one file answer
 elif [[ $# -eq 0 ]]
 then
   for d in [0-9]*
   do
-    input_file="./$d/input.txt"
     for p in a b
     do
-      if [[ ! -d "$d/$p" ]]
-      then
-        continue
-      fi
-      correct_answer=$(jq -r ".ans$d.$p" answers.json)
-      for f in "$d/$p"/*
-      do
-        test_one "$f" "$correct_answer" "$d" "$p"  # TODO: remove $3, $4
-      done
+      test_part "$d" "$p"
     done
   done
 else
